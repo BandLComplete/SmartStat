449828bf635dbddc0d57bc0a99b062c921d4729d
i.borovik <borovik.i@skbkontur.ru>
2021-09-30 11:19:00 +0500

    Init

---
+++ EntryPoint/Database/Context.cs
@@ -0,0 +1,22 @@
+using System;
+using System.IO;
+using Microsoft.EntityFrameworkCore;
+
+namespace EntryPoint.Database
+{
+	public class Context : DbContext
+	{
+		public DbSet<User> Users { get; set; }
+		
+		public string DbPath { get; private set; }
+
+		public Context()
+		{
+			var folder = Environment.CurrentDirectory;
+			DbPath = Path.Combine(folder, "smartStat.db");
+		}
+
+		protected override void OnConfiguring(DbContextOptionsBuilder options)
+			=> options.UseSqlite($"Data Source={DbPath}");
+	}
+}
