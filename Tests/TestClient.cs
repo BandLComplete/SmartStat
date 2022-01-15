532fcec4705c419c552c20f1959066221d3c25c7
BandLComplete <boroviki2000@mail.ru>
2022-01-15 09:47:04 +0500

    Tests

---
+++ Tests/TestClient.cs
@@ -0,0 +1,32 @@
+﻿namespace Tests;
+
+public class TestClient : Client
+{
+	protected override string Url => "https://localhost:5001";
+
+	public async Task<bool> Login(string name, string password)
+	{
+		return await SendPost<bool>(Api.Login, ToQueries(name, password));
+	}
+
+	public async Task<bool> Register(string name, string password)
+	{
+		return await SendPost<bool>(Api.Register, ToQueries(name, password));
+	}
+
+	public async Task<bool> DeleteUser(string name, string password)
+	{
+		return await SendPost<bool>(Api.DeleteUser, ToQueries(name, password));
+	}
+
+	private async Task<T> SendPost<T>(string method, params (string Name, string Value)[] queries)
+	{
+		var response = await client.PostAsync(AddQueries($"{Url}/{method}web", queries), null);
+		return await ReadBody<T>(response);
+	}
+
+	private static (string Name, string Value)[] ToQueries(string name, string password)
+	{
+		return new[] { ("name", name), ("password", password) };
+	}
+}
